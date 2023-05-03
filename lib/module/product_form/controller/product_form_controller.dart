dca3defc4c79605c0a2ea7df281c38759b9db649
dhanisworo <ahmad.dhanisworo@atomic.id>
2023-05-03 17:08:50 +0700

    Product
    product slicing

--- lib/module/product_form/controller/product_form_controller.dart
+++ lib/module/product_form/controller/product_form_controller.dart
@@ -1,10 +1,28 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:pos_app_skripsi/state_util.dart';
 import '../view/product_form_view.dart';
+
 
 class ProductFormController extends State<ProductFormView> implements MvcController {
   static late ProductFormController instance;
   late ProductFormView view;
+  File? _image;
+  
+
+  Future getImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if(image == null) return;
+
+    final imageTemporary = File(image.path);
+
+    setState(() {
+      this._image = imageTemporary;
+    });
+  }
+
 
   @override
   void initState() {
--- lib/module/product_form/view/product_form_view.dart
+++ lib/module/product_form/view/product_form_view.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:pos_app_skripsi/core.dart';
 import '../controller/product_form_controller.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProductFormView extends StatefulWidget {
   const ProductFormView({Key? key}) : super(key: key);
 
   Widget build(context, ProductFormController controller) {
     controller.view = this;
+    ImagePicker picker = new ImagePicker();
 
     return Scaffold(
       appBar: AppBar(
@@ -17,7 +21,70 @@ class ProductFormView extends StatefulWidget {
         child: Container(
           padding: const EdgeInsets.all(10.0),
           child: Column(
-            children: const [],
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Image.network("https://i.ibb.co/QrTHd59/woman.jpg"),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                  title: 'Pick from Gallery',
+                  icon: Icons.image_outlined,
+                  onClick: () => controller.getImage()),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                title: 'Pick from Camera',
+                icon: Icons.camera,
+                onClick: () => {},
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Product Name',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Price',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Description',
+                ),
+                keyboardType: TextInputType.multiline,
+                maxLines: 4,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.save),
+                label: const Text("save"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {},
+              ),
+              SizedBox(
+                height: 40,
+              ),
+            ],
           ),
         ),
       ),
@@ -26,4 +93,26 @@ class ProductFormView extends StatefulWidget {
 
   @override
   State<ProductFormView> createState() => ProductFormController();
+}
+
+Widget CustomButton({
+  required String title,
+  required IconData icon,
+  required VoidCallback onClick,
+}) {
+  return Container(
+    width: 280,
+    child: ElevatedButton(
+      onPressed: () => onClick,
+      child: Row(
+        children: [
+          Icon(icon),
+          SizedBox(
+            width: 20,
+          ),
+          Text(title)
+        ],
+      ),
+    ),
+  );
 }
