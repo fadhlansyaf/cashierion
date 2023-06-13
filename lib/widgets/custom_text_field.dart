5530ae40aab9445744c168e7f03a574df88fe7b3
dhanisworo <ahmad.dhanisworo@atomic.id>
2023-06-13 17:34:25 +0700

    Update custom_text_field.dart

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -9,14 +9,19 @@ class CustomTextFieldOld extends StatelessWidget {
       required this.controller,
       this.keyboardType,
       required this.label,
+      this.validation,
       this.helperText,
       this.onTap,
-      this.suffixIcon = const Icon(Icons.arrow_drop_down,color: ColorTheme.COLOR_PRIMARY,),
+      this.suffixIcon = const Icon(
+        Icons.arrow_drop_down,
+        color: ColorTheme.COLOR_PRIMARY,
+      ),
       this.onSaved})
       : super(key: key);
   final TextEditingController controller;
   final TextInputType? keyboardType;
   final String label;
+  final Function(String?)? validation;
   final String? helperText;
   final Function()? onTap;
   final Icon suffixIcon;
@@ -25,7 +30,11 @@ class CustomTextFieldOld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (onTap != null && controller.text.isEmpty) {
-      if (suffixIcon == Icon(Icons.arrow_drop_down,color: ColorTheme.COLOR_PRIMARY,)) {
+      if (suffixIcon ==
+          Icon(
+            Icons.arrow_drop_down,
+            color: ColorTheme.COLOR_PRIMARY,
+          )) {
         controller.text = 'Pilih salah satu';
       }
     }
@@ -37,6 +46,7 @@ class CustomTextFieldOld extends StatelessWidget {
         controller: controller,
         onTap: onTap,
         readOnly: onTap != null,
+        // validator: validation,
         cursorColor: ColorTheme.COLOR_PRIMARY,
         decoration: InputDecoration(
             helperText: helperText,
