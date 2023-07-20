95b58215871a150aa9062b56c848cc257b79d08e
dhanisworo <ahmad.dhanisworo@atomic.id>
2023-07-20 20:06:31 +0700

    Update export_excel.dart

--- lib/utils/export_excel.dart
+++ lib/utils/export_excel.dart
@@ -122,14 +122,6 @@ class FileStorage {
     return directory;
   }
 
-  static String getFilePath(String name) {
-    // final directory = await getApplicationDocumentsDirectory();
-    // return directory.path;
-    // To get the external path from device of download folder
-    final path = _localPath;
-    return '$path/$name';
-  }
-
   static Future<File> writeCounter(List<int> bytes, String name) async {
     final path = await _localPath;
     // Create a file for the path of
@@ -139,7 +131,58 @@ class FileStorage {
     print("Save file");
 
     // Write the data in the file you have created
-
     return file.writeAsBytes(bytes);
   }
 }
+
+// class FileStorage {
+//   static Future<String> getExternalDocumentPath() async {
+//     // To check whether permission is given for this app or not.
+//     var status = await Permission.storage.status;
+//     if (!status.isGranted) {
+//       // If not we will ask for permission first
+//       await Permission.storage.request();
+//     }
+//     Directory _directory = Directory("");
+//     if (Platform.isAndroid) {
+//       // Redirects it to download folder in android
+//       _directory = Directory("/storage/emulated/0/Download");
+//     } else {
+//       _directory = await getApplicationDocumentsDirectory();
+//     }
+
+//     final exPath = _directory.path;
+//     print("Saved Path: $exPath");
+//     await Directory(exPath).create(recursive: true);
+//     return exPath;
+//   }
+
+//   static Future<String> get _localPath async {
+//     // final directory = await getApplicationDocumentsDirectory();
+//     // return directory.path;
+//     // To get the external path from device of download folder
+//     final String directory = await getExternalDocumentPath();
+//     return directory;
+//   }
+
+//   static String getFilePath(String name) {
+//     // final directory = await getApplicationDocumentsDirectory();
+//     // return directory.path;
+//     // To get the external path from device of download folder
+//     final path = _localPath;
+//     return '$path/$name';
+//   }
+
+//   static Future<File> writeCounter(List<int> bytes, String name) async {
+//     final path = await _localPath;
+//     // Create a file for the path of
+//     // device and file name with extension
+
+//     File file = File('$path/$name');
+//     print("Save file");
+
+//     // Write the data in the file you have created
+
+//     return file.writeAsBytes(bytes);
+//   }
+// }
